ee852f0187034822714ac59d865641334b3c7807
Vinícius Costa <vinipbk@gmail.com>
2019-09-24 22:33:43 -0300

    Desktop finalizado

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 $(function(){
     $('.media__carousel').slick({
-        slidesToShow: 2,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: false,
         autoplaySpeed: 2000,
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
     });
 });
