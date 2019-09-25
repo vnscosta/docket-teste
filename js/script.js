cf71b7f490cdb7dcbe2cb84eeb54b262f2e51411
Vinícius Costa <vinipbk@gmail.com>
2019-09-25 03:24:58 -0300

    Versão mobile alterada

--- js/script.js
+++ js/script.js
@@ -8,15 +8,32 @@ $(function(){
             {
                 breakpoint: 1400,
                 settings: {
-                    slidesToShow: 2
+                    slidesToShow: 2,
                 }
             },
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 1
+                    slidesToShow: 1,
+                    arrows: false,
+                    dots: true,
                 }
             },
         ]
     });
 });
+$('.menu__mobile--btn').click( function(){
+    $('.menu__mobile--btn').toggleClass('active');
+    $('.menu').toggleClass('active');
+})
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ){
+    $(window).bind('scroll', function () {
+        if($(window).scrollTop() > 350){
+            $('nav').css('position','fixed');
+            $('nav').css('background-color','#5A5789');
+        }else{
+            $('nav').css('position','sticky');
+            $('nav').css('background-color','transparent');
+        };
+    })
+};
